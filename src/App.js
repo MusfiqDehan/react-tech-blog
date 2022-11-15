1b90605dd5768aec2bb0016490fcc95602e22579
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-15 23:26:31 +0600

    update to react-router version 5->6 and fix bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import Layout from "./components/Layout";
+import Home from "./components/Home";
+import NewPost from "./components/NewPost";
+import PostPage from "./components/PostPage";
+import About from "./components/About";
+import Missing from "./components/Missing";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [posts, setPosts] = useState([
+        {
+            id: 1,
+            title: "My First Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 2,
+            title: "My 2nd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 3,
+            title: "My 3rd Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+        {
+            id: 4,
+            title: "My Fourth Post",
+            datetime: "July 01, 2021 11:17:36 AM",
+            body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!",
+        },
+    ]);
+
+    const [search, setSearch] = useState("");
+
+    return (
+        <Routes>
+            <Route
+                path="/"
+                element={<Layout search={search} setSearch={setSearch} />}
+            >
+                <Route index element={<Home posts={posts} />} />
+                <Route path="post">
+                    <Route index element={<NewPost />} />
+                    <Route path=":id" element={<PostPage />} />
+                </Route>
+                <Route path="about" element={<About />} />
+                <Route path="*" element={<Missing />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
