1f08f79eec12929f342bec489406805ee6015f7d
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-17 14:17:37 +0600

    Add NewPost Page

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,7 +1,34 @@
-const NewPost = () => {
+import React from "react";
+
+const NewPost = (
+    handleSubmit,
+    postTitle,
+    setPostTitle,
+    postBody,
+    setPostBody
+) => {
     return (
-        <main>
-            <h1>NewPost</h1>
+        <main className="NewPost">
+            <h2>NewPost</h2>
+            <form className="newPostForm" onSubmit={handleSubmit}>
+                <label htmlFor="postTitle">Title:</label>
+                <input
+                    id="postTitle"
+                    type="text"
+                    required
+                    value={postTitle}
+                    onChange={(e) => setPostTitle(e.target.value)}
+                    placeholder="Write Title Here"
+                />
+                <label htmlFor="postBody">Post:</label>
+                <textarea
+                    id="postBody"
+                    required
+                    value={postBody}
+                    onChange={(e) => setPostBody(e.target.value)}
+                />
+                <button type="submit">Add Post</button>
+            </form>
         </main>
     );
 };
