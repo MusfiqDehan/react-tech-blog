ee516112d53ba91e1cf2b3f4f273e41a0d53dd80
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-17 21:54:07 +0600

    🌱 UPDATE: Missing filr

--- src/components/Missing.jsx
+++ src/components/Missing.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 const Missing = () => {
     return (
         <main>
-            <h1>Missing</h1>
+            <h2>Page Not Found</h2>
+            <p>Well, that is very disappointing.</p>
+            <Link to="/">Please visit our Homepage</Link>
         </main>
     );
 };
