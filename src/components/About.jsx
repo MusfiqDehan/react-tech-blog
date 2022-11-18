f47e048d64f0fde5aa04e9f3e2659b9f4ad15696
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-18 22:48:34 +0600

    Update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,10 @@
+import React from "react";
+
 const About = () => {
     return (
-        <main>
-            <h1>About</h1>
+        <main className="About">
+            <h2>About</h2>
+            <p>This is a simple react blog app.</p>
         </main>
     );
 };
