9e671024fcc569bfc6bba4ed0f16ab791689ef58
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-11-18 22:50:24 +0600

    Update layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
+import React from "react";
 import { Outlet } from "react-router-dom";
 
 const Layout = ({ search, setSearch }) => {
